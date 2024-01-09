90bf834782f40e42560732d0da364bc3cbf9d41d
Fatma Akacha <akachafatma1@gmail.com>
2024-01-09 10:12:24 +0000

    props

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
-function App() {
-  const chaine= <p> Bonjour react</p>
+function App(props) {
+  const nom = props.nom;
+  const age = props.age;
   return (
     <div className="App">
-      {chaine}
+     <p>Votre nom: {nom}</p>
+     <p>Votre age: {age}</p>
     </div>
   );
 }
