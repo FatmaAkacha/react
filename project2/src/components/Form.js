439a4a58c5f1e0919e1f0452d6ff2b4a1dd05697
Fatma Akacha <akachafatma1@gmail.com>
2024-01-16 09:28:26 +0000

    usestate

--- project2/src/components/Form.js
+++ project2/src/components/Form.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
 function Form(props) {
+    const [name, setName] = useState("");
     function handleSubmit(e) {
         e.preventDefault();
-        props.addTask("Dire bonjour !");
+props.addTask(name);
+setName("");
         }
+        function handleChange(e) {
+            setName(e.target.value);
+            }
     return (
         <form onSubmit={handleSubmit}>
             <h2 className="label-wrapper">
@@ -16,6 +22,8 @@ function Form(props) {
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
                 Ajouter
