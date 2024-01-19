f12a15412c459315820c3fdf1377bf7a1ae7c470
Fatma Akacha <akachafatma1@gmail.com>
2024-01-19 14:51:11 +0000

    result

--- project3/src/pages/Survey.js
+++ project3/src/pages/Survey.js
@@ -1,30 +1,12 @@
 import React from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import Results from './Results'; 
+import { useParams, Link } from 'react-router-dom';
+import Results from './Results';
 
 function Survey() {
     const { questionNumber } = useParams();
-    const navigate = useNavigate();
-
     const questionNumberInt = parseInt(questionNumber, 10);
 
-    const goToPreviousQuestion = () => {
-        const previousQuestionNumber = questionNumberInt - 1;
-        if (previousQuestionNumber >= 1) {
-            navigate(`/survey/${previousQuestionNumber}`);
-        }
-    };
-
-    const goToNextQuestion = () => {
-        const nextQuestionNumber = questionNumberInt + 1;
-        if (nextQuestionNumber <= 10) {
-            navigate(`/survey/${nextQuestionNumber}`);
-        } else {
-            navigate('/results');
-        }
-    };
-
-    if (questionNumberInt === 10) {
+    if (questionNumberInt === 11) {
         return <Results />;
     }
 
@@ -32,16 +14,21 @@ function Survey() {
         <div>
             <h1>Questionnaire 🧮</h1>
             {questionNumberInt === 1 && <h2>Question 1</h2>}
-            {questionNumberInt !== 1 && questionNumberInt !== 10 && (
+            {questionNumberInt !== 1 && questionNumberInt !== 11 && (
                 <h2>Question {questionNumber}</h2>
             )}
 
-            <button onClick={goToPreviousQuestion} disabled={questionNumberInt === 1}>
-                Previous
-            </button>
-            <button onClick={goToNextQuestion} disabled={questionNumberInt === 10}>
-                Next
-            </button>
+            {questionNumberInt > 1 && (
+                <Link to={`/survey/${questionNumberInt - 1}`}>
+                    Previous
+                </Link>
+            )}
+
+            {questionNumberInt < 11 && (
+                <Link to={`/survey/${questionNumberInt + 1}`}>
+                    Next
+                </Link>
+            )}
         </div>
     );
 }
