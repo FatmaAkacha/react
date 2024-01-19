6a35145af7e57392194902eaf987a906587ac789
Fatma Akacha <akachafatma1@gmail.com>
2024-01-19 12:16:52 +0000

    Rendu conditionnel

--- project2/src/components/Todo.js
+++ project2/src/components/Todo.js
@@ -47,26 +47,6 @@ function Todo(props) {
             </div>
         </div>
     );
-    return (
-        <li className="todo stack-small">
-            <div className="c-cb">
-                <input id={props.id} type="checkbox" defaultChecked={props.completed} />
-                <label className="todo-label" htmlFor={props.id}>
-                    {props.name}
-                </label>
-            </div>
-            <div className="btn-group">
-                <button type="button" className="btn">
-                    Éditer <span className="visually-hidden">{props.name}</span>
-                </button>
-                <button
-                    type="button"
-                    className="btn btn__danger"
-                    onClick={() => props.deleteTask(props.id)}>
-                    Supprimer <span className="visually-hidden">{props.name}</span>
-                </button>
-            </div>
-        </li>
-    );
+    return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
 export default Todo;
