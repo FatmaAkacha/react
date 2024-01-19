deba00734036c50a8ab1fdc3364d1d58a022550a
Fatma Akacha <akachafatma1@gmail.com>
2024-01-19 15:36:07 +0000

    outlet(route secondaire)

--- project3/src/pages/Results.js
+++ project3/src/pages/Results.js
@@ -1,7 +1,11 @@
+import { Outlet, Link } from 'react-router-dom'
 function Results() {
     return (
         <div>
             <h1>Résultats</h1>
+            <Link to="/initial">Résultat Initial</Link>
+            <Link to="/final">Résultat Final</Link>
+            <Outlet />
         </div>
     )
 }
