35d77ebe1e7b014197a912ea53d8db7e4967d646
Fatma Akacha <akachafatma1@gmail.com>
2024-01-19 13:22:16 +0000

    page error

--- project3/src/index.js
+++ project3/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
+import Error from './components/Error';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/survey" element={<Survey />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   </Router>
 );
