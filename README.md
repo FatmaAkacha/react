666c9b0eb21d56b2d25e7f4054bd6e16fea1cc9b
Fatma Akacha <akachafatma1@gmail.com>
2024-01-19 13:09:34 +0000

    router

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ npx create-react-app project1
 cd project1
 npm start
 npm install nanoid
+npm install --save react-router-dom
---
+++ project3/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/survey" element={<Survey />} />
+    </Routes>
+  </Router>
+);
