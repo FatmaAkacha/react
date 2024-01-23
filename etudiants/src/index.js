382100f761e8265946f3a1955b70285522984170
Fatma Akacha <akachafatma1@gmail.com>
2024-01-23 10:35:35 +0000

    formulaire

--- etudiants/src/index.js
+++ etudiants/src/index.js
@@ -3,7 +3,7 @@ import Recherche from './Recherche';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Form from './Form';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +11,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/recherche/:id" element={<Recherche />} />
+      <Route path="/form" element={<Form />} />
     </Routes>
   </Router>
 );
