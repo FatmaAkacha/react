fc5d4a4c5ebf04e6a238f70b4eb20d555f6dbefa
Fatma Akacha <akachafatma1@gmail.com>
2024-01-23 10:07:06 +0000

    existance des etudiants

--- etudiants/src/Recherche.js
+++ etudiants/src/Recherche.js
@@ -1,27 +1,36 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function Recherche() {
-    const [data, setData] = useState([]);
-    const {id} = useParams();
-    const EtudiantTotal = data.length;
-    useEffect(() => {
-        fetch(`https://3002-fatmaakacha-react-cck07jabxp3.ws-eu107.gitpod.io/etudiants/${id}`)
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => {
-          console.error(error);
-        });
-    }, []);
-    console.log(data);
-  
-    return (
-      <div>
-        <ul>
-            <li> {data.nom}</li>
-        </ul>
-  
-      </div>
-    );
+  const [data, setData] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://3002-fatmaakacha-react-cck07jabxp3.ws-eu107.gitpod.io/etudiants/${id}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Etudiant not found");
+        }
+        return response.json();
+      })
+      .then(data => setData(data))
+      .catch(error => {
+        console.error(error);
+        setData(null); 
+      });
+  }, [id]);
+
+  if (!data) {
+    return <div>Etudiant n'existe pas</div>;
   }
+
+  return (
+    <div>
+      <ul>
+        <li>{data.nom}</li>
+      </ul>
+    </div>
+  );
+}
+
 export default Recherche;
